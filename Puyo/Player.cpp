a979e12878b7f52d942cc0de4679f1b7a10cd7b2
sejimhp <sejimhp@gmail.com>
2015-06-14 14:03:02 +0900

    improve put puyo

--- Puyo/Player.cpp
+++ Puyo/Player.cpp
@@ -7,11 +7,14 @@ void Player::init(int X, int Y){
 	this->Y = Y;
 	count = 0;
 	rot = 270;
+	put_flag = 0;
+	rot_flag = 0;
 }
 
 void Player::run(){
 	input();
 
+	//minute action etc..
 	action();
 
 	//atact another player etc..
@@ -21,7 +24,20 @@ void Player::run(){
 void Player:: input(){
 	if (Input::KeyUp.clicked && rot_flag == 0){
 		//Rotaition
-		rot_flag = 1;
+		if (rot == 270){
+			if (x == 0 && (stage[x / SIZE][y / SIZE] != 0 || stage[x / SIZE][y / SIZE + 1] != 0)){}
+			else if (stage[x / SIZE - 1][y / SIZE] != 0 || stage[x / SIZE + 1][y / SIZE] != 0){}
+			else
+				rot_flag = 1;
+		}
+		else if (rot == 90){
+			if (x == 0 && (stage[x / SIZE][y / SIZE] != 0 || stage[x / SIZE][y / SIZE - 1] != 0)){}
+			else if (stage[x / SIZE - 1][y / SIZE + 1] != 0 || stage[x / SIZE + 1][y / SIZE + 1] != 0){}
+			else
+				rot_flag = 1;
+		}
+		else
+			rot_flag = 1;
 	}else if (Input::KeyDown.pressed){
 		//speedy
 		y += 2;
@@ -37,29 +53,107 @@ void Player:: input(){
 void Player::action(){
 	//Routaition
 	if (rot_flag == 1){
+		if (x == 0 && rot == 90)
+			x += 36;
+		else if (x == 7 * SIZE && rot == 270)
+			x -= 36;
 		rot += 5;
-		if (rot % 90 == 0)
+		if (rot % 90 == 0){
 			rot_flag = 0;
+			rot %= 360;
+		}
 	}
 	//put puyo
-	if (y >= 10 * SIZE || stage[x / SIZE][y / SIZE + 2] != 0){
+	if ((y >= 10 * SIZE || stage[x / SIZE][y / SIZE + 2] != 0) && rot == 270 && put_flag == 0){
+		//down
 		stage[x / SIZE][y / SIZE] = puyo[count][0];
 		stage[x / SIZE][y / SIZE + 1] = puyo[count][1];
+		put_flag = 1;
+	}
+	else if ((y >= 11 *  SIZE || stage[x / SIZE][y / SIZE + 1] != 0) && rot == 90 && put_flag == 0){
+		//up
+		stage[x / SIZE][y / SIZE] = puyo[count][0];
+		stage[x / SIZE][y / SIZE - 1] = puyo[count][1];
+		put_flag = 1;
+	}
+	else if ((y >= 11 *  SIZE || stage[x / SIZE][y / SIZE + 1] != 0 || stage[x / SIZE - 1][y / SIZE + 1] != 0) && rot == 180 && put_flag == 0){
+		//left
+		if (stage[x / SIZE][y / SIZE + 1] == 0){
+			//under main puyo
+			stage[x / SIZE - 1][y / SIZE] = puyo[count][1]; 
+			put_flag = 2;
+		}
+		else if (stage[x / SIZE - 1][y / SIZE + 1] == 0){
+			//under another puyo
+			stage[x / SIZE][y / SIZE] = puyo[count][0];
+			put_flag = 3;
+		}
+		else {
+			stage[x / SIZE][y / SIZE] = puyo[count][0];
+			stage[x / SIZE - 1][y / SIZE] = puyo[count][1];
+			put_flag = 1;
+		}
+	}
+	else if ((y >= 11 *  SIZE || stage[x / SIZE][y / SIZE + 1] != 0 || stage[x / SIZE + 1][y / SIZE + 1] != 0) && rot == 0 && put_flag == 0){
+		//right
+		if (stage[x / SIZE][y / SIZE + 1] == 0){
+			//under main puyo
+			stage[x / SIZE + 1][y / SIZE] = puyo[count][1]; 
+			put_flag = 2;
+		}
+		else if (stage[x / SIZE + 1][y / SIZE + 1] == 0){
+			//under another puyo
+			stage[x / SIZE][y / SIZE] = puyo[count][0];
+			put_flag = 3;
+		}
+		else {
+			stage[x / SIZE][y / SIZE] = puyo[count][0];
+			stage[x / SIZE + 1][y / SIZE] = puyo[count][1];
+			put_flag = 1;
+		}
+	}
+	else if (put_flag == 0)
+		y++;
+	if (put_flag != 0 && put() == 1){
+		put_flag = 0;
 		count++;
 		y = 0;
 		x = 3 * SIZE;
 		rot = 270;
 	}
-	y++;
 }
 
-void puyo_event(){
+int Player::put(){
+	if (put_flag == 2){
+		y += 3;
+		if (stage[x / SIZE][y / SIZE + 1] != 0 || y >= 11 * SIZE){
+			stage[x / SIZE][y / SIZE] = puyo[count][0];
+			return 1;
+		}
+		return 0;
+	}
+	else if (put_flag == 3){
+		y += 3;
+		if (rot == 0 && (stage[x / SIZE + 1][y / SIZE + 1] != 0 || y > 11 * SIZE)){
+			stage[x / SIZE + 1][y / SIZE] = puyo[count][1];
+			return 1;
+		}
+		else if (rot == 180 && (stage[x / SIZE - 1][y / SIZE + 1] != 0 || y > 11 * SIZE)){
+			stage[x / SIZE - 1][y / SIZE] = puyo[count][1];
+			return 1;
+		}
+		return 0;
+	}
+	return 1;
+}
+
+void Player::puyo_event(){
 
 }
 
 void Player::draw(){
-	//赤　青　黄　緑　紫
-	//置かれてるぷよ
+	//Red Blue Yellow Green Purple
+	//putting puyo
 	for (int i = 0; i < 8; i++){
 		for (int j = 0; j < 12; j++){
 			switch (stage[i][j]){
@@ -81,10 +175,14 @@ void Player::draw(){
 			}
 		}
 	}
-	//落下中のぷよ
+	//puyo is falling
 	for (int i = 0; i < 2; i++){
-		int xx = X + x + i * (cos((double)rot / 360 * 2 * PI)) * SIZE;
-		int yy = Y + y + i * (-sin((double)rot / 360 * 2 * PI)) * SIZE;
+		if (put_flag == 2 && i == 1)
+			break;;
+		if (put_flag == 3)
+			i++;
+		int xx = X + x + (int)(i * (cos((double)rot / 360 * 2 * PI)) * SIZE);
+		int yy = Y + y + (int)(i * (-sin((double)rot / 360 * 2 * PI)) * SIZE);
 		switch (puyo[count][i]){
 		case 1:
 			TextureAsset(L"puyo")((puyo[count][i] - 1) * 24, 0, 24, 24).scale(1.5).draw(xx, yy);
@@ -115,4 +213,5 @@ void Player::set(int data[][2]){
 			stage[i][j] = 0;
 		}
 	}
+	x = 3;
 }
--- Puyo/Player.h
+++ Puyo/Player.h
@@ -14,12 +14,17 @@ class Player{
 	int puyo[100][2];
 	//Rotaition
 	int rot, rot_flag;
+	//put
+	int put_flag;
 public:
 	void init(int, int);
+
 	void run();
 	void action();
+	int put();
 	void input();
 	void puyo_event();
+
 	void set(int data[][2]);
 	void draw();
 };
